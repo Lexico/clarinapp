c17904b16bbbf374d424e905087b62dcf2222d86
pablo.grabarnik@spieldev.com <pablo.grabarnik@spieldev.com>
2019-08-16 17:25:47 -0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,33 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+
+/*
+// @subtepass
+module.exports = {
+  consumer_key: 'XNtYYEL41AqSrAXM4FQHRGJ0n',  
+  consumer_secret: 'JZdKl1LpKtmAsUYtzpYtidnbSqYP4d9C4TYlFVTvozNzAQmqOy',
+  access_token: '68233502-9JtePEpAW74Z5oqzU2MA4OJG77Tg2iFRHxpAnJDrm',  
+  access_token_secret: 'WSnNq1dBQmGN13U8Sh4YZ9thD9owEn2GbfpJjG8UO9oOz'
+}
+*/
+/*
+// @littlemrwriter
+module.exports = {
+  consumer_key: 'FyNVIzMKCJ8I1Y1P26nZ4hh7R',  
+  consumer_secret: 'pkahmpMwCHOyOKFMyE50Y2NIpxstZ1hPU27beylWXHeZ5AkBBR',
+  access_token: '755034139099889664-JyQL5A9PpptvI2l3bz37irJ40hwxxLx',  
+  access_token_secret: 'ls0codxj58dcas3Na5PXcJFoBecWBrj5mmBmcE1PBR5nj'
+}
+*/
+// @subtepass
+module.exports = {
+  consumer_key: 'piT1xrkMnW08JyQnZgHdg',
+  consumer_secret: '91rhSoydfq8AOisnZyDb5zRWiq4dGGeXI4b6BtGUA',
+  access_token: '68233502-6bbDAXulEeTT0PKJHphyBPCM3fcHbND9jzLPQJVhM',
+  access_token_secret: 'E0z5y1z7CXtziADrKR8IR8EQ6nPBJu8SCJpGanLq37tka'
+}
